2d59742aa3d85ea7b6ab584fb0ebadfdbbc7d5a5
yakupgit <137324492+ykpgunduz@users.noreply.github.com>
2025-06-28 16:59:18 +0300

    edited

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yagunduz <yagunduz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 13:44:37 by yagunduz          #+#    #+#             */
-/*   Updated: 2025/06/28 15:05:55 by yagunduz         ###   ########.fr       */
+/*   Updated: 2025/06/28 16:34:20 by yagunduz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	i;
 	size_t	slen;
 
 	if (!s)
 		return (NULL);
-	slen = 0;
-	while (s[slen] != '\0')
-		slen++;
+	slen = ft_strlen(s);
 	if (start >= slen)
-		return ((char *)malloc(1));
+		return (ft_strdup(""));
 	if (len > slen - start)
 		len = slen - start;
-	sub = (char *)malloc(len + 1);
+	sub = malloc(len + 1);
 	if (!sub)
 		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		sub[i] = s[start + i];
-		i++;
-	}
-	sub[i] = '\0';
+	ft_memcpy(sub, s + start, len);
+	sub[len] = '\0';
 	return (sub);
 }
